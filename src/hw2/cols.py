1af21e42c03f4ac6f71fa9ad46004558e1e077ef
aramasw <aramasw@ncsu.edu>
2023-01-26 11:39:48 -0500

    Added data and modified Col

--- src/hw2/cols.py
+++ src/hw2/cols.py
@@ -23,14 +23,16 @@ class Cols:
         self.y = []
         self.klass = None
         for n, s in enumerate(t):
-            col = Num(n, s) if s[0].isupper() else Sym(n, s)
+            col = Num(n, s) if re.findall("^[A-Z]+", s) else Sym(n, s)
             self.all.append(col)
-            if s[-1] != 'X':
-                if s[-1] == '!':
+            if not re.findall("X$", s):
+                if re.findall("!$", s):
                     self.klass = col
-                if s.find('+') != -1 or s.find('-') != -1:
-                    self.y.append(col)
-                else:
-                    self.x.append(col)
+                self.y.append(col) if re.findall("[!+-]$", s) else self.x.append(col)
+
+    def add(self, row: Row):
+        for _, t in enumerate(self.x, self.y):
+            for _, col in enumerate(t):
+                col.add(row.cells[col.at])
 
 
--- src/hw2/data.py
+++ src/hw2/data.py
@@ -1,6 +1,6 @@
-from cols import Cols
-from row import Row
-from utilities import Utilities
+from src.cols import Cols
+from src.row import Row
+from src.utilities import kap, map, csv
 import math
 
 class Data:
@@ -13,13 +13,41 @@ class Data:
     cols = summary of data
     rows = list of rows
     '''
-    def __init__(self, src, fun):
+    def __init__(self, src):
         self.rows = []
         self.cols = None
-        self.src=src
-
         if isinstance(src, str):
-            utilities.csv(src, fun)
+            csv(src, self.add)
         else:
-            for row in enumerate(src or []):
-                self.add(row)
+            map(src or {},self.add) 
+
+    def add(self, t):
+        '''
+        Adds a row and appends to any columns
+        :param t: row to add
+        '''
+        if self.cols:
+           t = t.cells and t or Row(t)
+           self.rows.append(t)
+           self.cols.add(t)
+        else:
+            self.cols = Cols(t)
+
+    def clone(self, init, data):
+        data = DATA(list(self.cols.names))
+        map(init or [], self.add)
+        return data
+
+    def stats(self, cols, nPlaces, what="mid"):
+        '''
+        Shows stats for each col based on input function
+        :param places: rounding int
+        :param cols: list of columns
+        :param what: function to apply to each column
+        :return: dictionary of column stats
+        '''
+        def fun(k, col):
+            return col.rnd((col,what)(),nplaces), col.txt
+        return kap(cols or self.cols.y, fun)
+
+
